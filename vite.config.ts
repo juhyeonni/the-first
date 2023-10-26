7eaa48e2ede3d7dd4e546e9f2a7a6edf94684ac7
Juhyeon Lee <l.juhyeonni@gmail.com>
2023-10-27 00:39:44 +0900

    ✨feat(config): Add absolute address configuration for Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: [
+      {
+        find: '@components',
+        replacement: '/src/components',
+      },
+      {
+        find: '@context',
+        replacement: '/src/context',
+      },
+      {
+        find: '@provider',
+        replacement: '/src/provider',
+      },
+      {
+        find: '@assets',
+        replacement: '/src/assets',
+      },
+      {
+        find: '@hooks',
+        replacement: '/src/hooks',
+      },
+      {
+        find: '@pages',
+        replacement: '/src/pages',
+      },
+      {
+        find: '@services',
+        replacement: '/src/services',
+      },
+      {
+        find: 'interfaces',
+        replacement: '/src/interfaces',
+      },
+      {
+        find: '@',
+        replacement: '/src',
+      },
+    ],
+  },
+});
