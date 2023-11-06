e3689142827ba5b755efd4a0d91b88ed04ed21cd
Juhyeon Lee <l.juhyeonni@gmail.com>
2023-11-06 14:16:44 +0900

    🔨 chore: styled components 의 글로벌 스타일 제공 및 리셋 css 추가

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  :root {
+    --nav-narrow-width: 72px; 
+    --nav-medium-width: 256px;
+  }
+`;
+
+export default GlobalStyle;
