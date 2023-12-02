7b596eaaf78fb1c7f7ced17160983a2b66b1993e
Juhyeon Lee <l.juhyeonni@gmail.com>
2023-12-02 17:41:27 +0900

    ✨ feat: assign Sidebar to the Left section

--- src/components/Left/index.tsx
+++ src/components/Left/index.tsx
@@ -1,11 +1,21 @@
 import styled from 'styled-components';
+import Sidebar from './Sidebar';
 
 const Left = () => {
-  return <Container>Left</Container>;
+  return (
+    <Container>
+      <Sidebar />
+    </Container>
+  );
 };
 
 export default Left;
 
 const Container = styled.div`
   flex: 1;
+  display: flex;
+  flex-direction: column;
+
+  /* FIXME: */
+  border: 1px solid red;
 `;
