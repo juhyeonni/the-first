0be15a9b8fb1793bb5a04c96b0e60283227ab45b
cheiru94 <cheiru94@gmail.com>
2023-12-04 16:46:57 +0900

    ✨ feat: MainCard 위치 정리

--- src/components/Main/MainCard.tsx
+++ src/components/Main/MainCard.tsx
@@ -1,8 +1,8 @@
-/* eslint-disable react/jsx-props-no-spreading */
-/* eslint-disable react/prop-types */
+/* --------------------------------------import-------------------------------------- */
 import React, { useState, Component } from 'react';
 import styled, { css, keyframes } from 'styled-components';
 
+/* 폰트 어썸 */
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // eslint-disable-next-line import/no-extraneous-dependencies
@@ -16,11 +16,185 @@ import {
   faComment,
 } from '@fortawesome/free-regular-svg-icons';
 
-// 캐러셀
+/* 캐러셀 */
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Slider, { Settings } from 'react-slick';
 
+/*  */
+import { PostAndUser } from '@interfaces/post.interface';
+
+interface MainCardProps {
+  post: PostAndUser;
+}
+/* --------------------------------------import-------------------------------------- */
+
+/* -------------------------------------MainCard------------------------------------- */
+const MainCard = ({ post }: MainCardProps): JSX.Element => {
+  /*  📝 사용자 게시글 입력 */
+  const textContent =
+    '★ブルーノート東京35周年 特設サイトオープン！インタビュー映像のフルバージョンは、こちらでご覧いただけます。ブルーノート東京35周年 特設サイトオープン！インタビュー映像のフルバージョンは、こちらでご覧いただけます。';
+  const maxLength = 30; // 원하는 글자 수
+
+  /* 📂 게시글 flug */
+  const [isTextShown, setIsTextShown] = useState(false);
+
+  /* 📂 하트 flug  */
+  const [isHeartShown, setIsHeartShown] = useState(false);
+
+  /* 📂 바운스 flug */
+  const [bounce, setBounce] = useState(false);
+
+  const heartFlug = () => {
+    setIsHeartShown((prev) => {
+      return !prev;
+    });
+  };
+
+  return (
+    <StyledMainCard>
+      {/* 🟢 1. 상단 bar 🟢 */}
+      <div className="element-top">
+        {/* 1.1 상단 좌측 유저 이미지 */}
+        <div className="element-image">
+          <img /* 🟡 사용자 이미지 입력 🟡  */
+            className="element-userImg"
+            alt="Element userImg"
+            src="/main_imgs/blue_giant.png"
+          />
+        </div>
+
+        {/* 1.2 상단 좌측 아이디 */}
+        <div className="element-account">
+          <span className="userId">
+            cheiru94 {/* <- 🟡 사용자 아이디 입력 🟡 */}
+            {/* 1.2.1 파란색 체크 이미지 */}
+            <img
+              className="element-userImg"
+              alt="Element userImg"
+              src="/main_imgs/blue_check.png"
+              style={{ width: '30px', height: '30px', marginLeft: '10px' }}
+            />
+          </span>
+        </div>
+
+        {/* 1.3 우측 상단 점 3개 */}
+        {/* <img
+          className="element-threeDot"
+          alt="Element threeDot"
+          src="/main_imgs/three_dot.png"
+        /> */}
+        <FontAwesomeIcon
+          className="faEllipsis"
+          icon={faEllipsis}
+          style={{ color: '#000000' }}
+        />
+      </div>
+
+      {/* 🟢 2. 메인 이미지 🟢 */}
+      <div className="mainImg_container">
+        {/* 2.1 캐러셀 */}
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <StyledSlider {...settings}>
+          {/* 🟡 map 메서드로 , 게시 사진 수 만큼 생성 🟡 */}
+          <div className="mainImg_box">
+            <img
+              className="element-userImg"
+              alt="Element userImg"
+              src="/main_imgs/blue_note1.png"
+            />
+          </div>
+          <div className="mainImg_box">
+            <img
+              className="element-userImg"
+              alt="Element userImg"
+              src="/main_imgs/blue_note2.png"
+            />
+          </div>
+          <div className="mainImg_box">
+            <img
+              className="element-userImg"
+              alt="Element userImg"
+              src="/main_imgs/blue_note3.jpeg"
+            />
+          </div>
+          <div className="mainImg_box">
+            <img
+              className="element-userImg"
+              alt="Element userImg"
+              src="/main_imgs/blue_note4.jpeg"
+            />
+          </div>
+        </StyledSlider>
+      </div>
+
+      {/* 🟢 3. 좋아요  + 게시글 모달 🟢 */}
+      <div className="element-wrap-image">
+        {/* 3.1 좋아요  */}
+        {isHeartShown ? (
+          /* 3.1.1 ♡ */
+          <FontAwesomeIcon
+            bounce={bounce}
+            className="solidHeart"
+            icon={solidHeart}
+            onClick={() => {
+              heartFlug();
+            }}
+          />
+        ) : (
+          /* 3.1.2 ❤️ */
+          <StyledSolidHeart
+            className="regularHeart"
+            icon={regularHeart}
+            onClick={() => {
+              heartFlug();
+              setBounce(true);
+              setTimeout(() => setBounce(false), 1000);
+            }}
+          />
+        )}
+
+        {/* 3.2  🔍 */}
+        <FontAwesomeIcon
+          className="comment"
+          icon={faComment}
+          flip="horizontal"
+        />
+      </div>
+
+      {/* 🟢 4. 아이디 + 게시글 작성 내용 🟢 */}
+      <div className="element-content">
+        {/* 4.1 사용자 아이디 */}
+        {/* 🟡 사용자 아이디 입력 🟡 */}
+        <span className="userId">cheiru94</span>
+
+        {/* 4.2 게시글 내용 */}
+        <span className="element-contents">
+          {isTextShown
+            ? textContent
+            : `${textContent.substring(0, maxLength)}...`}
+          <button type="button" onClick={() => setIsTextShown(!isTextShown)}>
+            {isTextShown ? '접기' : '더보기'}
+          </button>
+        </span>
+      </div>
+
+      {/* 🟢 5. 댓글 달기  🟢 */}
+      <div className="element-comment">
+        <div className="element">
+          <textarea className="text-wrapper" placeholder="댓글 달기..." />
+        </div>
+        <button type="button" className="element-upload">
+          게시
+        </button>
+      </div>
+      {/* <div className="frame" /> */}
+    </StyledMainCard>
+  );
+};
+/* -------------------------------------MainCard------------------------------------- */
+
+/* -------------------------------------Styled Component------------------------------------- */
 //  바운스 키프레임
 const bounceAnimation = keyframes`
   0%, 100% {
@@ -319,168 +493,6 @@ const StyledSlider = styled(Slider)`
     }
   }
 `; /* 🟡 캐러셀 스타일링 終🟡 */
-
-/* ------------------------------------------------------  MainCard ------------------------------------------------------  */
-const MainCard = (): JSX.Element => {
-  /*  📝 사용자 게시글 입력 */
-  const textContent =
-    '★ブルーノート東京35周年 特設サイトオープン！インタビュー映像のフルバージョンは、こちらでご覧いただけます。ブルーノート東京35周年 特設サイトオープン！インタビュー映像のフルバージョンは、こちらでご覧いただけます。';
-  const maxLength = 30; // 원하는 글자 수
-
-  /* 📂 게시글 flug */
-  const [isTextShown, setIsTextShown] = useState(false);
-
-  /* 📂 하트 flug  */
-  const [isHeartShown, setIsHeartShown] = useState(false);
-
-  /* 📂 바운스 flug */
-  const [bounce, setBounce] = useState(false);
-
-  const heartFlug = () => {
-    setIsHeartShown((prev) => {
-      return !prev;
-    });
-  };
-
-  return (
-    <StyledMainCard>
-      {/* 🟢 1. 상단 bar 🟢 */}
-      <div className="element-top">
-        {/* 1.1 상단 좌측 유저 이미지 */}
-        <div className="element-image">
-          <img /* 🟡 사용자 이미지 입력 🟡  */
-            className="element-userImg"
-            alt="Element userImg"
-            src="/main_imgs/blue_giant.png"
-          />
-        </div>
-
-        {/* 1.2 상단 좌측 아이디 */}
-        <div className="element-account">
-          <span className="userId">
-            cheiru94 {/* <- 🟡 사용자 아이디 입력 🟡 */}
-            {/* 1.2.1 파란색 체크 이미지 */}
-            <img
-              className="element-userImg"
-              alt="Element userImg"
-              src="/main_imgs/blue_check.png"
-              style={{ width: '30px', height: '30px', marginLeft: '10px' }}
-            />
-          </span>
-        </div>
-
-        {/* 1.3 우측 상단 점 3개 */}
-        {/* <img
-          className="element-threeDot"
-          alt="Element threeDot"
-          src="/main_imgs/three_dot.png"
-        /> */}
-        <FontAwesomeIcon
-          className="faEllipsis"
-          icon={faEllipsis}
-          style={{ color: '#000000' }}
-        />
-      </div>
-
-      {/* 🟢 2. 메인 이미지 🟢 */}
-      <div className="mainImg_container">
-        {/* 2.1 캐러셀 */}
-        <StyledSlider {...settings}>
-          {/* 🟡 map 메서드로 , 게시 사진 수 만큼 생성 🟡 */}
-          <div className="mainImg_box">
-            <img
-              className="element-userImg"
-              alt="Element userImg"
-              src="/main_imgs/blue_note1.png"
-            />
-          </div>
-          <div className="mainImg_box">
-            <img
-              className="element-userImg"
-              alt="Element userImg"
-              src="/main_imgs/blue_note2.png"
-            />
-          </div>
-          <div className="mainImg_box">
-            <img
-              className="element-userImg"
-              alt="Element userImg"
-              src="/main_imgs/blue_note3.jpeg"
-            />
-          </div>
-          <div className="mainImg_box">
-            <img
-              className="element-userImg"
-              alt="Element userImg"
-              src="/main_imgs/blue_note4.jpeg"
-            />
-          </div>
-        </StyledSlider>
-      </div>
-
-      {/* 🟢 3. 좋아요  + 게시글 모달 🟢 */}
-      <div className="element-wrap-image">
-        {/* 3.1 좋아요  */}
-        {isHeartShown ? (
-          /* 3.1.1 ♡ */
-          <FontAwesomeIcon
-            bounce={bounce}
-            className="solidHeart"
-            icon={solidHeart}
-            onClick={() => {
-              heartFlug();
-            }}
-          />
-        ) : (
-          /* 3.1.2 ❤️ */
-          <StyledSolidHeart
-            className="regularHeart"
-            icon={regularHeart}
-            onClick={() => {
-              heartFlug();
-              setBounce(true);
-              setTimeout(() => setBounce(false), 1000);
-            }}
-          />
-        )}
-
-        {/* 3.2  🔍 */}
-        <FontAwesomeIcon
-          className="comment"
-          icon={faComment}
-          flip="horizontal"
-        />
-      </div>
-
-      {/* 🟢 4. 아이디 + 게시글 작성 내용 🟢 */}
-      <div className="element-content">
-        {/* 4.1 사용자 아이디 */}
-        {/* 🟡 사용자 아이디 입력 🟡 */}
-        <span className="userId">cheiru94</span>
-
-        {/* 4.2 게시글 내용 */}
-        <span className="element-contents">
-          {isTextShown
-            ? textContent
-            : `${textContent.substring(0, maxLength)}...`}
-          <button type="button" onClick={() => setIsTextShown(!isTextShown)}>
-            {isTextShown ? '접기' : '더보기'}
-          </button>
-        </span>
-      </div>
-
-      {/* 🟢 5. 댓글 달기  🟢 */}
-      <div className="element-comment">
-        <div className="element">
-          <textarea className="text-wrapper" placeholder="댓글 달기..." />
-        </div>
-        <button type="button" className="element-upload">
-          게시
-        </button>
-      </div>
-      {/* <div className="frame" /> */}
-    </StyledMainCard>
-  );
-};
+/* -------------------------------------Styled Component------------------------------------- */
 
 export default MainCard;
