684e0118d753c4ac29b448fe08d4afaf2a618bdc
hetame1 <poi8946@naver.com>
2023-12-09 16:27:05 +0900

    ✨ feat: 로그인 페이지

---
+++ src/pages/login.tsx
@@ -0,0 +1,118 @@
+import Button from '@components/Button';
+import Input from '@components/Input';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {};
+
+  return (
+    <Main>
+      <Container>
+        <Body>
+          <Header>TheFirst</Header>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <Input
+              id="username"
+              type="text"
+              placeholder="사용자 이름"
+              register={register}
+              required
+              errors={errors}
+              minLength={3}
+            />
+
+            <Input
+              id="password"
+              type="password"
+              placeholder="비밀번호"
+              register={register}
+              required
+              errors={errors}
+              minLength={6}
+            />
+
+            <Button label="로그인" />
+          </Form>
+        </Body>
+        <Bottom>
+          <BottomText>
+            계정이 없으신가요? <Link to="/register">가입하기</Link>
+          </BottomText>
+        </Bottom>
+      </Container>
+    </Main>
+  );
+};
+
+export default LoginPage;
+
+const Main = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Container = styled.div`
+  max-width: 400px;
+`;
+
+const Body = styled.div`
+  border: 1px solid black;
+  border-radius: 2px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Header = styled.div`
+  text-align: center;
+  margin-top: 36px;
+  margin-bottom: 12px;
+  font-size: 28px;
+  font-weight: 700;
+  line-height: 32px;
+  font-style: italic;
+`;
+
+const Form = styled.form`
+  width: 350px;
+  margin: 0 2.5rem;
+`;
+
+const Bottom = styled.div`
+  margin-top: 1rem;
+  padding: 1rem;
+  border: 1px solid black;
+  border-radius: 2px;
+  width: 100%;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+`;
+
+const BottomText = styled.div`
+  line-height: 32px;
+
+  a {
+    margin-left: 0.25rem;
+    color: #0095f6;
+    font-weight: 600;
+  }
+`;
