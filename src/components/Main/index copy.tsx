dd36acfe8e3e754bf9f5ea7acbc9b79410c4ab98
cheiru94 <cheiru94@gmail.com>
2023-12-10 23:22:48 +0900

    원본 복사용

---
+++ src/components/Main/index copy.tsx
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+import { PostAndUser, Post } from '@interfaces/post.interface';
+import Story from './Story';
+import MainCard from './MainCard';
+// eslint-disable-next-line import/order
+import { getPosts, getPostsUsers } from '@services/posts.service';
+
+const Main = () => {
+  const [mainPosts, setMainPosts] = useState<PostAndUser[]>([]);
+  const [onlyPosts, setOnlyPosts] = useState<Post[]>([]);
+
+  /* post + user 데이터 가져오기  */
+  useEffect(() => {
+    getPostsUsers()
+      .then((posts) => {
+        // console.log('getPostsUsers :', posts); // 가져온 포스트 출력
+        setMainPosts(posts);
+      })
+      .catch((err) => {
+        // console.error(err); // 에러 발생 시 출력
+      });
+  }, []);
+
+  /* post만 */
+  useEffect(() => {
+    const getPostsFun = async () => {
+      const result = await getPosts();
+      // console.log('getPostsFun :', result);
+      setOnlyPosts(result);
+    };
+    getPostsFun();
+  }, []);
+
+  return (
+    <Container>
+      <Story />
+      {/* 메인 카드 작성 */}
+      <div style={{ margin: '0 auto' }}>
+        {[...mainPosts].reverse().map((post, index) => (
+          <MainCard key={post.id} post={post} onlyPost={onlyPosts[index]} />
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default Main;
+
+const Container = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+`;
---
+++ src/services/posts  copy.service.ts
@@ -0,0 +1,46 @@
+import { baseAxios } from '@axios';
+import {
+  Post,
+  PostPayload,
+  PostAndUser,
+  User,
+} from '@interfaces/post.interface';
+import { createTags } from './tags.service';
+
+export async function createPost(post: PostPayload) {
+  if (post.tags) createTags(post.tags);
+
+  const res = await baseAxios.post('/posts', {
+    ...post,
+    created_at: Date.now(),
+    updated_at: Date.now(),
+  });
+}
+
+export async function getPosts(): Promise<Post[]> {
+  const res = await baseAxios.get('/posts');
+  return res.data;
+}
+
+// 🟡 posts와 users를 관계 쿼리 : posts와 users 같이 나옴 🟡
+// src / components / Main / index.tsx 에서 사용 중
+export async function getPostsUsers(): Promise<PostAndUser[]> {
+  // 반환하는 형은 Promise이고, 그 형태를 PostAndUser인터페이스 형식으로 맞추겠다
+  const res = await baseAxios.get('/posts?_expand=user');
+  // console.log('posts.service.ts에서 출력됨!!!!', res.data);
+  return res.data;
+}
+
+// 🟡 하트 patch 🟡
+// src / components / Main / MainCard.tsx 에서 사용 중
+export async function patchHeart(post: {
+  id: number;
+  heart: boolean;
+}): Promise<PostAndUser[]> {
+  // eslint-disable-next-line prefer-template
+  // const res = await axios.patch('/posts/1', heart);
+  const res = await baseAxios.patch(`/posts/${post.id}`, post);
+  return res.data;
+}
+
+export async function getHearts() {}
